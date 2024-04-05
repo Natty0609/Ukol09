beecc9d092e6aa5d03e679a279a8b4c827012be9
Nataliya Tahova <ntahova@gmail.com>
2024-04-05 15:10:18 +0200

    ukol09

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-console.log('Funguju!')
-
 const karticky = document.querySelectorAll('.karticka')
 
-karticky[0].classList.remove('otocena')
+karticky.forEach(karta => {
+    karta.addEventListener('click', () => {
+        karta.classList.toggle('otocena')
+
+    })
+});
+
+
+// karticky[0].classList.remove('otocena')
